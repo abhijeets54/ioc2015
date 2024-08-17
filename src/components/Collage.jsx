12115f391560af89d1c2265346907e18cf6f7e84
abhijeets54 <116050986+abhijeets54@users.noreply.github.com>
2024-08-17 17:24:58 +0530

    first commit

--- src/components/Collage.jsx
+++ src/components/Collage.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaInstagram, FaFacebookF, FaYoutube } from 'react-icons/fa'; // Importing icons
 
 function Collage() {
   const navigate = useNavigate();
 
   const handleDonateClick = (e) => {
-    e.stopPropagation();  // Prevent event propagation
+    e.stopPropagation(); // Prevent event propagation
     navigate('/Donate'); // Make sure this path matches your routing setup
   };
 
   const handleJoinUsClick = (e) => {
-    e.stopPropagation();  // Prevent event propagation
+    e.stopPropagation(); // Prevent event propagation
     navigate('/JoinUs'); // Make sure this path matches your routing setup
+  };
+
+  const handleSocialClick = (url) => {
+    window.open(url, '_blank');
   };
 
   return (
@@ -62,17 +67,39 @@ function Collage() {
             />
           </div>          
         </div>
-        {/* Removed the carousel-control-prev and carousel-control-next buttons */}
       </div>
 
       <div className="absolute inset-0 flex items-center bg-black bg-opacity-50">
         <div className="flex flex-col items-start ml-10 text-white">
           <h4 className="text-5xl mt-1 font-medium text-left mx-44">Creating Leaders,
             <br /> Transforming Lives</h4>
+          
           <div className="buttons flex flex-row mt-1 mx-44">
             <button className="m-2 px-4 py-2 rounded-3xl bg-orange-500 text-white" onClick={handleDonateClick}>Donate now</button>
             <button className="m-2 px-4 py-2 rounded-3xl border-2 border-white bg-transparent text-white" onClick={handleJoinUsClick}>Join us</button>
           </div>
+        </div>
+
+        {/* Social Media Buttons */}
+        <div className="absolute right-10 top-1/2 transform -translate-y-1/2 flex flex-col space-y-4">
+          <button
+            className="w-10 h-10 flex items-center justify-center rounded-full bg-orange-500 hover:bg-gray-600 text-white"
+            onClick={() => handleSocialClick('https://www.instagram.com/initiatorsofchange_')}
+          >
+            <FaInstagram />
+          </button>
+          <button
+            className="w-10 h-10 flex items-center justify-center rounded-full  bg-orange-500 hover:bg-gray-600 text-white"
+            onClick={() => handleSocialClick('https://www.facebook.com/initiatorsofchange')}
+          >
+            <FaFacebookF />
+          </button>
+          <button
+            className="w-10 h-10 flex items-center justify-center rounded-full  bg-orange-500 hover:bg-gray-600 text-white"
+            onClick={() => handleSocialClick('https://www.youtube.com/@initiatorsofchange5173')}
+          >
+            <FaYoutube />
+          </button>
         </div>
       </div>
     </div>
